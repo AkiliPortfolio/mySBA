388773cf2e3e6805efb0d0d81e7ccea158150182
AkiliPortfolio <145647661+AkiliPortfolio@users.noreply.github.com>
2023-10-12 15:11:56 +0800

    upload 12 oct

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ thumbnails.forEach(function(thumbnail) {
    const thumbnailSrc = this.getAttribute('src');
    const videoTitle = this.getAttribute('alt');
    const videoDescription = this.getAttribute('data-description');
-   video.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+   video.setAttribute<iframe width="560" height="315" src="https://www.youtube.com/embed/zaNh3dClwxI?si=D5l1KqzmmgkFxdb0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
    videoInfo.querySelector('h2').textContent = videoTitle;
    videoInfo.querySelector('p').textContent = videoDescription;
    thumbnails.forEach(function(thumbnail) {
